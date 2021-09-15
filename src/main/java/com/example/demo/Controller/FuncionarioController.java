f967cd716f10eddfd94f9ae7b8090bed5a39d83c
PedroHenrique-ueg <84983523+PedroHenrique-ueg@users.noreply.github.com>
2021-09-14 22:20:25 -0300

    as
    as

--- src/main/java/com/example/demo/Controller/FuncionarioController.java
+++ src/main/java/com/example/demo/Controller/FuncionarioController.java
@@ -70,4 +70,6 @@ public class FuncionarioController {
 			
 			return funcRep.pegamatricula();
 		}
+		
+		
 }
--- src/main/java/com/example/demo/Model/Cliente.java
+++ src/main/java/com/example/demo/Model/Cliente.java
@@ -1,5 +1,6 @@
 package com.example.demo.Model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,6 +19,7 @@ public class Cliente {
 	
 	private String nome;
 	
+	@Column(unique=true)
 	private String cgc;
 	
 	@JsonFormat(pattern="dd/MM/YYYY")
